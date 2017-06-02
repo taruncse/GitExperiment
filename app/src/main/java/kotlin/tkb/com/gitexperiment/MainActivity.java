cb7762a3dea03a807397d95610c89c7aad19a065
Tarun <tarun.pstu@gmail.com>
2017-06-02 17:36:12 +0600

    Added two variable

--- app/src/main/java/kotlin/tkb/com/gitexperiment/MainActivity.java
+++ app/src/main/java/kotlin/tkb/com/gitexperiment/MainActivity.java
@@ -2,6 +2,7 @@ package kotlin.tkb.com.gitexperiment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is for experiment
+
+        int a=5 ,b=5;
+        Log.i("Tag: "," The sum is: "+a+b);
     }
 }
